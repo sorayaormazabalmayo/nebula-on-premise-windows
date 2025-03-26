0eb407995b46a7a6f303502ae218111605989d59
s.ormazabal <s.ormazabal@saltosystems.com>
2025-03-26 13:46:20 +0100

    Nebula-on-premise version 13

--- internal/server/server.go
+++ internal/server/server.go
@@ -33,7 +33,7 @@ type UpdateStatus struct {
 }
 
 var (
-	jsonFilePath = "/home/sormazabal/src/SALTO2/update_status.json"
+	jsonFilePath = "/home/sormazabal/src/SALTO-client-linux/update_status.json"
 	updateStatus UpdateStatus
 	updateMutex  sync.Mutex
 )
